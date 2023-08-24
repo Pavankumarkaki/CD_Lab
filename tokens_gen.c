958742dc984e1f7c0f69603d0a35542aab552b6a
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2023-08-24 14:22:44 +0530

    Update tokens_gen.c
    deleted duplicate keyword

--- tokens_gen.c
+++ tokens_gen.c
@@ -39,12 +39,11 @@ bool isKeyword(char* str)
 {
 if (!strcmp(str, "if") || !strcmp(str, "else") ||
 !strcmp(str, "while") || !strcmp(str, "do") ||
-!strcmp(str, "break") ||
-!strcmp(str, "continue") || !strcmp(str, "int")
+!strcmp(str, "break") ||!strcmp(str, "continue") ||
+!strcmp(str, "int")
 || !strcmp(str, "double") || !strcmp(str, "float")
 || !strcmp(str, "return") || !strcmp(str, "char")
-|| !strcmp(str, "case") || !strcmp(str, "char")
-|| !strcmp(str, "sizeof") || !strcmp(str, "long")
+|| !strcmp(str, "case")|| !strcmp(str, "sizeof") || !strcmp(str, "long")
 || !strcmp(str, "short") || !strcmp(str, "typedef")
 || !strcmp(str, "switch") || !strcmp(str, "unsigned")
 || !strcmp(str, "void") || !strcmp(str, "static")
